6302c811c33f0940e609b64536504bace5c3cf19
Andrii Brodskii <andriibrodskii@MacBook-Air-Andrii.local>
2024-02-23 10:22:22 +0200

    Adding Swipe to the project.

---
+++ Assets/Core/Swipe/Scripts/Extensions/Vector2Extension.cs
@@ -0,0 +1,18 @@
+using Core.Swipe.Scripts.Data;
+using UnityEngine;
+
+namespace Core.Swipe.Scripts.Extensions
+{
+    internal static class Vector2Extension
+    {
+        public static Direction Vector2ToDirection(this ref Vector2 vector)
+        {
+            if (Mathf.Abs(vector.x) > Mathf.Abs(vector.y))
+            {
+                return vector.x > 0 ? Direction.Right : Direction.Left;
+            }
+
+            return vector.y > 0 ? Direction.Up : Direction.Down;
+        }
+    }
+}
---
+++ Assets/Core/Swipe/Scripts/Implementation/SwipeDetector.cs
@@ -0,0 +1,47 @@
+using System;
+using Core.Swipe.Scripts.Data;
+using Core.Swipe.Scripts.Extensions;
+using UnityEngine;
+
+namespace Core.Swipe.Scripts.Implementation
+{
+    public class SwipeDetector : MonoBehaviour
+    {
+        private const float MinSwipeMagnitude = 100;
+
+        public event Action<Direction> SwipeEnded;
+
+        private bool _isTouch;
+        private Vector2 _startPosition;
+        
+        private void Update()
+        {
+            if (!_isTouch && Input.GetMouseButtonDown(0))
+            {
+                _isTouch = true;
+                _startPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+
+            if (!_isTouch || !Input.GetMouseButtonUp(0))
+            {
+                return;
+            }
+            
+            var finishPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            _isTouch = false;
+            
+            OnSwipeEnded(_startPosition, finishPosition);
+        }
+        
+        private void OnSwipeEnded(Vector2 startPosition, Vector2 finishPosition)
+        {
+            var swipeDirection = finishPosition - startPosition;
+            if (swipeDirection.magnitude < MinSwipeMagnitude)
+            {
+                return;
+            }
+            
+            SwipeEnded?.Invoke(swipeDirection.Vector2ToDirection());
+        }
+    }
+}
