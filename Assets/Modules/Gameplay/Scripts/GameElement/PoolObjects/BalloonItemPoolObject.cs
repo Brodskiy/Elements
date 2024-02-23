bcc23a4ff0dc6eb0b4b668d489412fb44699d1e2
Andrii Brodskii <andriibrodskii@MacBook-Air-Andrii.local>
2024-02-23 15:01:06 +0200

    Randomized balloon flight

--- Assets/Modules/Gameplay/Scripts/GameElement/PoolObjects/BalloonItemPoolObject.cs
+++ Assets/Modules/Gameplay/Scripts/GameElement/PoolObjects/BalloonItemPoolObject.cs
@@ -10,21 +10,25 @@ namespace Modules.Gameplay.Scripts.GameElement.PoolObjects
         public event Action<BalloonItemPoolObject> OutForBoard;
         
         private float _speed;
-        private float _amplitude = 2f;
-        private float _frequency = 1f;
-        private float _rotationAmount = 10f;
+        private float _amplitude;
+        private float _frequency;
+        private float _rotationAmount;
         private float _startingY;
         private Camera _mainCamera;
 
         private void Awake()
         {
             _mainCamera = Camera.main;
-            _speed = Random.Range(0.3f, 1.5f);
         }
 
         public void StartFly()
         {
-            _startingY = Random.Range(0, 3);
+            _speed = Random.Range(0.3f, 1.5f);
+            _startingY = Random.Range(0, 3f);
+            _rotationAmount = Random.Range(2f, 10f);
+            _amplitude = Random.Range(0.6f, 1.5f);
+            _frequency = Random.Range(1f, 2f);
+            
             var initialPosition = transform.position;
             initialPosition.x = -_mainCamera.orthographicSize * _mainCamera.aspect - 0.5f;
             initialPosition.y = _startingY;
@@ -33,7 +37,7 @@ namespace Modules.Gameplay.Scripts.GameElement.PoolObjects
 
         private void Update()
         {
-            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+            transform.Translate(Vector3.right * (_speed * Time.deltaTime));
             var newY = _startingY + Mathf.Sin(Time.time * _frequency) * _amplitude;
             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
             var rotationAngle = Mathf.Sin(Time.time * _frequency) * _rotationAmount;
